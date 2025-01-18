42cb79f4423f390d4f6efa80b057a7684348464c
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-01-19 01:45:36 +0530

    responsiveness done

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -64,11 +64,11 @@ export default function PlanPage() {
 
     return (
         <section className=" ">
-            <div className="flex backdrop-blur-xl inset-0 bg-opacity-5  items-center justify-center flex-col gap-5 h-screen">
+            <div className="flex backdrop-blur-xl bg-opacity-5  items-center justify-center flex-col gap-5 h-screen">
                 <div className="flex backdrop-blur-sm items-center justify-center gap-2 mt-5 px-3 py-1 rounded border border-gray-500" >
                     <CiHome /> <Link href={"/"} >Home</Link>  {">"} <IoBulbOutline /> <Link href={"/plan"} >Plan</Link>
                 </div>
-                <form onSubmit={handelPlan} className="space-y-4 w-[500px] border border-gray-700 px-10 py-8 rounded-md">
+                <form onSubmit={handelPlan} className="space-y-4 w-[500px] border border-gray-700 px-10 py-8 rounded-md max-sm:w-auto max-sm:px-5">
                     <h1 className="text-4xl font-semibold pb-5 animate-pulse text-center">Lets Plan For You!!</h1>
                     {nextStep === 1 && <div>
                         <div className="mb-2">
@@ -149,9 +149,14 @@ export default function PlanPage() {
                         <input className="text-black px-4 py-2 w-full" required placeholder="short term goal" value={shortTermGoal} onChange={(e) => { setShortTermGoal(e.target.value) }} type="text" />
                     </div>}
                     <div className="flex items-center justify-start gap-5">
-                        <button onClick={() => setNextStep(nextStep - 1)} className={`px-4 py-1 rounded bg-neutral-800 group ${nextStep === 1 ? "hidden" : "block"}`}><span className="flex items-center gap-2"><FcPrevious className="group-hover:-translate-x-1 transition-transform ease-in-out duration-200" />Prev </span></button>
+                        <button onClick={() => setNextStep(nextStep - 1)} className={`px-4 py-1 rounded bg-neutral-800 group ${nextStep === 1 ? "hidden" : "block"}`}>
+                            <span className="flex items-center gap-2"><FcPrevious className="group-hover:-translate-x-1 transition-transform ease-in-out duration-200" />Prev </span>
+                        </button>
 
-                        <button onClick={() => setNextStep(nextStep + 1)} className={`px-4 py-1 rounded bg-neutral-800 group ${nextStep === 5 ? "hidden" : "block"}`}><span className="flex items-center gap-2">Next <FcNext className="group-hover:translate-x-1 transition-transform ease-in-out duration-200" /></span></button>
+                        <button onClick={() => setNextStep(nextStep + 1)} className={`px-4 py-1 rounded bg-neutral-800 group ${nextStep === 5 ? "hidden" : "block"}`}>
+                            <span className="flex items-center gap-2">Next <FcNext className="group-hover:translate-x-1 transition-transform ease-in-out duration-200" />
+                            </span>
+                        </button>
                     </div>
                     {nextStep === 5 && <button type="submit" className="px-5 my-10 w-full py-2 bg-neutral-700 rounded uppercase font-semibold">{loading ? <Loader title="Creating your plan" /> : "Plan It"}</button>}
                 </form>
@@ -160,7 +165,7 @@ export default function PlanPage() {
             {
                 data.length === 0 ? "" : data?.map(({ time, task, whatWillItHelpToAchieve }) => {
                     return <section className="flex items-center justify-center " key={Math.random()}>
-                        <div className="w-[500px] flex items-center justify-center flex-col">
+                        <div className="w-[500px] max-sm:w-auto max-sm:px-5 flex items-center justify-center flex-col">
                             <div className=" border p-4 w-full rounded">
                                 <h1 className="font-semibold text-lg uppercase py-2">Time 🕖 : {time} </h1>
                                 <h1 className="font-semibold antialiased py-2 text-lg ">Task : <span>{task}</span></h1>
